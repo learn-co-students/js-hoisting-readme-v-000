3d13126245c8b478b8d3ec2f247916758d37323b
vicfriedman <victoria.m.friedman@gmail.com>
2016-01-06 11:46:21 -0500

    lab

--- hoisting.js
+++ hoisting.js
@@ -9,11 +9,22 @@ function callMe(){
   var lyric = "maybe";
 }
 
+
 function crazy(){
+  //fix the code in here:
   thisIsCrazy();
-  
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
 
+}
+
+function sayMyName(){
+  //fix the code in here:
+  var name = "Cricky";
+  sayMy();  
+  function sayMy(){
+    console.log(name);
+    var name = "Kristin";
+  } 
 }
