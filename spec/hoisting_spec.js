62f4647539c873b2769200b228ab8779eaaa7bed
Charles Pletcher <cpletcher2@gmail.com>
2016-03-22 09:46:13 -0400

    Fix test description
    Fixes #6

--- spec/hoisting_spec.js
+++ spec/hoisting_spec.js
@@ -1,14 +1,14 @@
 'use strict';
 
 describe('callMe', function(){
-  
+
   it("returns the string 'maybe' ", function(){
     expect(callMe()).toEqual("maybe");
   });
 });
 
 describe('crazy', function(){
-  
+
   it("prints 'hey!!!' to the console from the nested function ", function(){
     spyOn(console, 'log');
     crazy();
@@ -17,8 +17,8 @@ describe('crazy', function(){
 });
 
 describe('sayMyName', function(){
-  
-  it("prints 'hey!!!' to the console from the nested function ", function(){
+
+  it("prints 'Kristin' to the console from the nested function ", function(){
     spyOn(console, 'log');
     sayMyName();
     expect(console.log).toHaveBeenCalledWith("Kristin");
