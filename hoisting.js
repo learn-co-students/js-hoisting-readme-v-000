4144df053962ff288c85ef84562315ca7fce9f8e
Alvinluipo <lualvin1234@gmail.com>
2016-03-22 00:27:44 -0400

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,19 +1,20 @@
 'use strict';
 
 function callMe(){
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+  
 }
 
 
 function crazy(){
   //fix the code in here:
   thisIsCrazy();
-  var thisIsCrazy = function (){
+  function thisIsCrazy() {
     console.log("hey!!!")
   }
 
@@ -23,8 +24,9 @@ function sayMyName(){
   //fix the code in here:
   var name = "Cricky";
   sayMy();  
-  function sayMy(){
-    console.log(name);
+  function sayMy() {
     var name = "Kristin";
+    console.log(name);
+    
   } 
 }
