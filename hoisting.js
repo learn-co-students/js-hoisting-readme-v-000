7f35e500c5d9991b7c8b7387891cc6d1af9d0173
empireofryan <rjohnson2001@gmail.com>
2016-03-24 02:31:30 -0400

    Done.

--- hoisting.js
+++ hoisting.js
@@ -5,26 +5,29 @@ function callMe(){
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
+
   var lyric = "maybe";
+  return lyric;
 }
 
 
 function crazy(){
   //fix the code in here:
-  thisIsCrazy();
+
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 
 }
 
 function sayMyName(){
   //fix the code in here:
   var name = "Cricky";
+  var name = "Kristin";
   sayMy();  
   function sayMy(){
     console.log(name);
-    var name = "Kristin";
+
   } 
 }
