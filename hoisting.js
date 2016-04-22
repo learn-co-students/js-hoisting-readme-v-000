152e047635b9b728b56b45ec5ad4b03b85273958
Smulligan85 <sean.mulligan85@gmail.com>
2016-04-21 18:33:58 -0700

    Finish hoisting README

--- hoisting.js
+++ hoisting.js
@@ -5,18 +5,19 @@ function callMe(){
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
+
 }
 
 
 function crazy(){
   //fix the code in here:
-  thisIsCrazy();
+  
   var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
-
+    console.log("hey!!!");
+  };
+  thisIsCrazy();
 }
 
 function sayMyName(){
@@ -24,7 +25,7 @@ function sayMyName(){
   var name = "Cricky";
   sayMy();  
   function sayMy(){
+      var name = "Kristin";
     console.log(name);
-    var name = "Kristin";
   } 
 }
