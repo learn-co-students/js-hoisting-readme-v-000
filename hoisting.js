d0c32dc3fe87f2e169fe0f3c03261499b7f6135c
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-05-03 12:52:34 -0500

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,32 @@
 'use strict';
 
+var lyric = "maybe";
+
 function callMe(){
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
+  return lyric ;
 }
 
 
 function crazy(){
   //fix the code in here:
   thisIsCrazy();
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
 
 }
+
+var thisIsCrazy = function (){
+    console.log("hey!!!")
+  }
 
 function sayMyName(){
   //fix the code in here:
   var name = "Cricky";
   sayMy();  
   function sayMy(){
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   } 
 }
