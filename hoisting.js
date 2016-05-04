20dc704dd3962e427500911678296030fdbdd029
Brian Holland <brian.holland@proatria.com>
2016-05-03 23:27:35 -0500

    Done.

--- hoisting.js
+++ hoisting.js
@@ -5,26 +5,24 @@ function callMe(){
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
 
 
 function crazy(){
-  //fix the code in here:
   thisIsCrazy();
-  var thisIsCrazy = function (){
+  function thisIsCrazy(){
     console.log("hey!!!")
   }
 
 }
 
 function sayMyName(){
-  //fix the code in here:
   var name = "Cricky";
   sayMy();  
   function sayMy(){
+    name = "Kristin";
     console.log(name);
-    var name = "Kristin";
   } 
 }
