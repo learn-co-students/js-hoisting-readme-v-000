bea1444d482796d39974ffe09ce6a15ab4545ac5
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-07-11 16:45:49 +0000

    done

--- hoisting.js
+++ hoisting.js
@@ -3,18 +3,18 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  var lyric = "maybe";  
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
-  }
+  }  
+  
+  thisIsCrazy();
 }
 
 function sayMyName() {
@@ -24,7 +24,7 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
+    var name = "Kristin";    
     console.log(name);
-    var name = "Kristin";
   }
 }
