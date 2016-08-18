6046d557a2bc813c57207ceb255fee00e56ad08a
Chris-Sexton <champlain2013@gmail.com>
2016-08-18 12:35:36 -0400

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,9 +1,9 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  var lyric = "maybe";
   return lyric;
 }
 
