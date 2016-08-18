d282ab2f94b3af885ccb71a40b521b4f5e913e27
Chris-Sexton <champlain2013@gmail.com>
2016-08-18 12:33:48 -0400

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,28 +3,26 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
 
 
 function crazy() {
-  // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
+  function thisIsCrazy(){
     console.log("hey!!!")
   }
 }
 
 function sayMyName() {
-  // fix the code in here:
   var name = "Cricky";
 
   sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
 }
