84b4ba702369da933f7e87057452cd9323d46e6a
Jason <jasonkwong11@gmail.com>
2016-08-23 17:56:18 -0400

    done

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,28 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
-
 function crazy() {
-  // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
+  function thisIsCrazy  (){
     console.log("hey!!!")
   }
 }
 
 function sayMyName() {
-  // fix the code in here:
+  
   var name = "Cricky";
+  var name = "Kristin";
 
   sayMy();
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
   }
 }
