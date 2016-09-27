5497b68920b95c9bef89c756a028ef2d6d892335
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-09-26 19:20:22 -0700

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,18 +1,18 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
   // fix the code in here:
   thisIsCrazy();
-
-  var thisIsCrazy = function (){
+  
+  function thisIsCrazy (){
     console.log("hey!!!")
   }
 }
@@ -20,11 +20,11 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
+  name = "Kristin";
   sayMy();
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
+    
   }
 }
