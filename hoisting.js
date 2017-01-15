829cbf835accc6c61b41f127d7c2ca4793c52031
bmolina-nyc <bruce.molina.81@gmail.com>
2017-01-15 17:41:18 -0500

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,9 +1,9 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  var lyric = "maybe";
   return lyric;
 }
 
@@ -21,12 +21,14 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
+
+  sayMy();
+
    function sayMy() {
     var name = "Kristin";
     console.log(name);
   }
 
-  sayMy();
 
  
 }
