6d5111df37927b60ddda1d54f0b26e4314869485
kkterai <kkterai@gmail.com>
2017-01-31 21:55:06 -0800

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,26 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
-  // fix the code in here:
+  function sayMy() {
+    var name = "Kristin";
+    console.log(name);
+  }
   var name = "Cricky";
 
   sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
 }
