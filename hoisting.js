e3bd486cc474e7b3f276b6f13830ed6e580bc930
Elias Ventura <eliasventura23@gmail.com>
2017-06-21 18:07:04 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,18 +3,19 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
+    return crazy()()
   }
+
 }
 
 function sayMyName() {
@@ -24,7 +25,9 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
+
     var name = "Kristin";
+    console.log(name);
   }
 }
+/
