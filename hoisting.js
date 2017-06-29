4e922d1b7bc385368cdd98a195cff060e05cea80
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-06-29 11:31:56 -0400

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,20 +1,24 @@
+// No sane human should ever write this code, but it works
+
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  lyric = "maybe"; //Wth javascript
   return lyric;
   var lyric = "maybe";
 }
 
 
 function crazy() {
+  thisIsCrazy = function (){
+    console.log("hey!!!")
+  }
   // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
+  var thisIsCrazy;
 }
 
 function sayMyName() {
@@ -24,6 +28,7 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
+    name = "Kristin";
     console.log(name);
     var name = "Kristin";
   }
