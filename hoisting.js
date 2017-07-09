62f5d76b0534b0b14493a7c39f5f4c87bf546e6a
Minh Nguyen <minhpcd@gmail.com>
2017-07-09 22:16:01 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,19 +1,19 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
+  // fix the code in here
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
+  var thisIsCrazy ="hey!!!";
+  {  console.log(thisIsCrazy)
   }
 }
 
@@ -24,7 +24,7 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
-  }
+    console.log(name);
+    }
 }
