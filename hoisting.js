be9a82b878f72a89456058b9d9e0260135a25e12
Brad Carlson <b2carlso@yahoo.com>
2017-07-29 16:05:42 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,32 @@
+callMe();
+
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  lyric = "maybe";
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
-  // fix the code in here:
+
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
+  function thisIsCrazy() {
+    console.log("hey!!!");
   }
 }
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
+  name = "Kristin";
 
   sayMy();
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
+    name = "Kristin";
   }
 }
