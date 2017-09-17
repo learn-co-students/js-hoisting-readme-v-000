f56bdb40ff6ccae678aee06bca685d6b569dc01d
Tom Su <djtomblue@gmail.com>
2017-09-17 16:33:54 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,8 +3,8 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
 
 
@@ -12,7 +12,7 @@ function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
+  function thisIsCrazy() {
     console.log("hey!!!")
   }
 }
