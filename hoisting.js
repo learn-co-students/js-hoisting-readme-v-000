5902394544edff3179623f7bdeeb61881f6481c6
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-24 23:44:23 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,23 @@
+var lyric = "maybe";
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
 
-  var thisIsCrazy = function (){
     console.log("hey!!!")
-  }
+  
 }
 
+var name = "Kristin";
 function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
+console.log(name);
 
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
 }
