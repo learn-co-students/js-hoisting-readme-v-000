825875ccfc6822894fd2671f8f52aa49a43a8a0b
amelie <amelieoller@gmail.com>
2018-01-31 09:13:03 -0800

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,30 @@
 function callMe() {
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
+	var lyric = 'maybe';
+
+	console.log('I just met you...');
+	console.log('and this is crazy..');
+	console.log("but here's my number..");
+	console.log('so call me');
+	return lyric;
 }
 
-
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
+	// fix the code in here:
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
+	var thisIsCrazy = function() {
+		console.log('hey!!!');
+	};
+	thisIsCrazy();
 }
 
 function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
+	// fix the code in here:
+	var name = 'Cricky';
 
-  sayMy();
+	sayMy();
 
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
+	function sayMy() {
+		var name = 'Kristin';
+		console.log(name);
+	}
 }
