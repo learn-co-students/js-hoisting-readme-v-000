95126eabcffef45fccb2c3754534142b07c35e1a
Michael Pieschel <mpieschel39@gmail.com>
2019-06-16 20:31:32 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,25 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
-  // fix the code in here:
   var name = "Cricky";
-
   sayMy();
-
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
 }
